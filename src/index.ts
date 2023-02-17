41d00e296bdaf41e714ea759ad600752a574b7b8
kalrashivam <shivamkalra2017@gmail.com>
2023-02-17 21:39:26 +0530

    feat: created a node api for data

---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import http from 'http';
+import { create, SdkConfig, SdkPool } from "@connext/sdk";
+import { ethers, Wallet, BigNumberish } from "ethers";
+import url from 'url';
+
+let mnemonic: string;
+if (!process.env.MNEMONIC) {
+    throw new Error("Please set your MNEMONIC in a .env file");
+} else {
+    mnemonic = process.env.MNEMONIC;
+}
+
+const connextDomain: { [key: number]: number} = {
+    5: 1735353714,
+    420: 1735356532,
+    80001: 9991,
+    421613: 421613
+}
+
+enum ChainId {
+    GOERLI = 5,
+    OPTIMISM = 420,
+    MUMBAI = 80001,
+    ARBITRUM = 421613
+}
+
+export const providerUrls: Record<number, string> = {
+    [ChainId.OPTIMISM]: 'https://opt-goerli.g.alchemy.com/v2/ZIZkXJUsv7NZ0UkYgtXhVT3IikhTqjNq',
+    [ChainId.GOERLI]: 'https://goerli.infura.io/v3/2a03dbc0d02945709182ed26b52cffd5',
+    [ChainId.MUMBAI]: 'https://polygon-mumbai.g.alchemy.com/v2/LWVjQQnv7tndzlugPWpFMDxDG--F7NYH',
+    [ChainId.ARBITRUM]: 'https://arb-goerli.g.alchemy.com/v2/4roOugVBSjCuI0vXsAWVR4c__GN40Z0v'
+}
+
+export const ConnextWeth: { [key: number]: string} = {
+    5: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+    420: "0x74c6FD7D2Bc6a8F0Ebd7D78321A95471b8C2B806",
+    80001: "0xFD2AB41e083c75085807c4A65C0A14FDD93d55A9",
+    421613: "0x1346786E6A5e07b90184a1Ba58E55444b99DC4A2"
+}
+
+type returnData ={
+    relayerFee: number,
+    originSlippage: number,
+    destinationSlippage: number
+}
+
+const getRelayerFee = async (fromChain:number, toChain:number, amount:number): Promise<returnData> => {
+    // Instantiate a Wallet object using your private key (i.e. from Metamask) and use it as a Signer.
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/1`);
+
+    // Connext to a Provider on the sending chain. You can use a provider like Infura (https://infura.io/) or Alchemy (https://www.alchemy.com/).
+    const originDomain = connextDomain[fromChain];
+    const destinationDomain = connextDomain[toChain];
+
+    const provider = new ethers.providers.JsonRpcProvider(providerUrls[fromChain]);
+    const signer = wallet.connect(provider);
+    const signerAddress = await signer.getAddress();
+
+    // Construct the `SdkConfig`. You can reference chain IDs in the "Resources" tab of the docs.
+    const sdkConfig: SdkConfig = {
+        signerAddress: signerAddress,
+        network: "testnet", // can be "mainnet" or "testnet"
+        chains: {
+                [originDomain]: {
+                providers: [providerUrls[fromChain]],
+            },
+                [destinationDomain]: {
+                providers: [providerUrls[toChain]],
+            },
+        },
+    };
+
+    const {sdkBase, sdkPool} = await create(sdkConfig);
+
+    // Estimate the relayer fee
+    const relayerFee = (await sdkBase.estimateRelayerFee({originDomain: originDomain.toString(), destinationDomain: destinationDomain.toString()})).toString();
+    const {amountReceived, originSlippage, routerFee, destinationSlippage} = (await sdkPool.calculateAmountReceived(
+                                                                                                    originDomain.toString(),
+                                                                                                    destinationDomain.toString(),
+                                                                                                    ConnextWeth[fromChain].toString(),
+                                                                                                    amount
+                                                                                                ));
+
+    const returnData = {
+        relayerFee: Number(relayerFee),
+        originSlippage: Number(originSlippage),
+        destinationSlippage: Number(destinationSlippage)
+    }
+
+    return returnData;
+}
+
+
+const myServer = http.createServer(async (req, res) => {
+    const query = url.parse(String(req.url), true).query;
+    await getRelayerFee(Number(query.fromChain), Number(query.toChain), Number(query.amount)).then((data) => {
+        const jsonData = JSON.stringify(data)
+        res.write(jsonData);
+    }).catch(() => {
+        res.write("Error")
+    });
+
+    res.end()
+});
+
+myServer.listen(3000, () => {
+    console.log('Server is running on port 3000. Go to http://localhost:3000/')
+});
+
+// myServer.close()
